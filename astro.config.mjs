e52f0b9028160e3305cbfa1b91b0f3dac21af366
2005WUYU <wuyu051019@qq.com>
2026-04-07 16:35:17 +0800

    latex

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 import react from '@astrojs/react';
 
@@ -10,4 +12,8 @@ import react from '@astrojs/react';
 export default defineConfig({
     site: 'https://example.com',
     integrations: [mdx(), sitemap(), react()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
